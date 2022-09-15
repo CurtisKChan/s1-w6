2c96bad6b68279059968323ed138e93ae6268388
sean-morris <sean.smorris@gmail.com>
2022-09-15 18:30:08 +0000

    Week 6

---
+++ LeapYear.java
@@ -0,0 +1,13 @@
+/**
+ * Complete the setLeapYear method to properly detect leap years.
+ */
+public class LeapYear {
+    // write this
+    // return true if year is divisible by 4, unless it is
+    // also divisible by 100 and not divisible by 400.
+    // Otherwise return false
+    public boolean isLeapYear(int year) {
+       return false;
+    }
+  }
+  
