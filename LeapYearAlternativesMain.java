817b8c7d97f94ab28465eacd4491296375a94a6d
sean-morris <sean.smorris@gmail.com>
2024-09-18 19:53:46 +0000

    Fixed Tests

--- LeapYearAlternativesMain.java
+++ LeapYearAlternativesMain.java
@@ -1,36 +1,33 @@
 public class LeapYearAlternativesMain {
     public static void main(String[] args) {
       LeapYearAlternatives lp = new LeapYearAlternatives();
-      System.out.println("You should have 8 leap years for each method: 2092, 2096, 2104, 2108, 2112, 2116, 2120, 2124");
+      System.out.println("You should have 4 leap years for each method: 2021, 2024, 2100, 2400");
       System.out.println("My Method");
-      for(int year = 2090; year < 2125; year++){
+      int tests = {2021, 2024, 2100, 2400}
+      for(int year: tests){
           boolean isLeapYear = lp.method(year);
-          if(isLeapYear)
-            System.out.println(year + ":" + isLeapYear);
+          System.out.println(year + ":" + isLeapYear);
       }
 
       System.out.println("");
       System.out.println("Method 1");
-      for(int year = 2090; year < 2125; year++){
+      for(int year: tests){
           boolean isLeapYear = lp.method1(year);
-          if(isLeapYear)
-            System.out.println(year + ":" + isLeapYear);
+          System.out.println(year + ":" + isLeapYear);
       }
 
       System.out.println("");
       System.out.println("Method 2");
-      for(int year = 2090; year < 2125; year++){
+      for(int year: tests){
           boolean isLeapYear = lp.method2(year);
-          if(isLeapYear)
-            System.out.println(year + ":" + isLeapYear);
+          System.out.println(year + ":" + isLeapYear);
       }
        
       System.out.println("");
       System.out.println("Method 3")
-      for(int year = 2090; year < 2125; year++){
+      for(int year: tests){
           boolean isLeapYear = lp.method3(year);
-          if(isLeapYear)
-            System.out.println(year + ":" + isLeapYear);
+          System.out.println(year + ":" + isLeapYear);
       }
     }
   }
