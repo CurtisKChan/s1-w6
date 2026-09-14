57c7a46531b00dfc299afbbd33494077395d46a4
CurtisKChan <chancurtisk@gmail.com>
2026-09-14 18:49:11 +0000

    9/14

--- StringPractice.java
+++ StringPractice.java
@@ -18,14 +18,13 @@ public class StringPractice {
     //BUT it doesn't fix it and then test it in
     //StringPracticeMain
     public String brokenEnd(String s){
-        return s.substring(0, 2) + (s.length() - 1) + s.substring(s.length() - 2);
+        return s.substring(0, 1) + (s.length()) + s.substring(s.length() - 1, s.length());
     }
 
     //This method should return first letter of
     //the parameter, b and the last two letters of the parameter, b
     //concatenated --> Ex. b = BeReal --> returns --> Bal
     public String firstLetterLastTwo(String b){
-        //write this
-        return null;
+        return b.substring(0,1) + b.substring(s.length() - 2);
     }
 }
